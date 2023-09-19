7bc59c92ae903e3b2c6d273ee5e7953b2a6160d1
Christian Stauffer <116654419+christian-stauffer@users.noreply.github.com>
2023-09-19 16:32:11 +0200

    Initial commit

---
+++ apiservices/api_configuration_service.go
@@ -0,0 +1,56 @@
+//  This file is part of the eliona project.
+//  Copyright © 2022 LEICOM iTEC AG. All Rights Reserved.
+//  ______ _ _
+// |  ____| (_)
+// | |__  | |_  ___  _ __   __ _
+// |  __| | | |/ _ \| '_ \ / _` |
+// | |____| | | (_) | | | | (_| |
+// |______|_|_|\___/|_| |_|\__,_|
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package apiservices
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"template/apiserver"
+)
+
+// ConfigurationApiService is a service that implements the logic for the ConfigurationApiServicer
+// This service should implement the business logic for every endpoint for the ConfigurationApi API.
+// Include any external packages or services that will be required by this service.
+type ConfigurationApiService struct {
+}
+
+// NewConfigurationApiService creates a default api service
+func NewConfigurationApiService() apiserver.ConfigurationApiServicer {
+	return &ConfigurationApiService{}
+}
+
+// GetConfigurations - Get example configurations
+func (s *ConfigurationApiService) GetConfigurations(ctx context.Context) (apiserver.ImplResponse, error) {
+	// TODO - update GetConfigurations with the required logic for this service method.
+	// Add api_configuration_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
+
+	//TODO: Uncomment the next line to return response Response(200, []Configuration{}) or use other options such as http.Ok ...
+	//return Response(200, []Configuration{}), nil
+
+	return apiserver.Response(http.StatusNotImplemented, nil), errors.New("GetConfigurations method not implemented")
+}
+
+// PostConfiguration - Creates an example configuration
+func (s *ConfigurationApiService) PostConfiguration(ctx context.Context, configuration apiserver.Configuration) (apiserver.ImplResponse, error) {
+	// TODO - update PostConfiguration with the required logic for this service method.
+	// Add api_configuration_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
+
+	//TODO: Uncomment the next line to return response Response(201, Configuration{}) or use other options such as http.Ok ...
+	//return Response(201, Configuration{}), nil
+
+	return apiserver.Response(http.StatusNotImplemented, nil), errors.New("PostConfiguration method not implemented")
+}
---
+++ go.mod
@@ -0,0 +1,40 @@
+module template
+
+go 1.20
+
+require (
+	github.com/eliona-smart-building-assistant/app-integration-tests v0.0.5
+	github.com/eliona-smart-building-assistant/go-eliona v1.9.7
+	github.com/eliona-smart-building-assistant/go-utils v1.0.30
+	github.com/gorilla/mux v1.8.0
+	github.com/volatiletech/sqlboiler/v4 v4.14.2
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+// Bugfix see: https://github.com/volatiletech/sqlboiler/blob/91c4f335dd886d95b03857aceaf17507c46f9ec5/README.md
+// decimal library showing errors like: pq: encode: unknown type types.NullDecimal is a result of a too-new and broken version of the github.com/ericlargergren/decimal package, use the following version in your go.mod: github.com/ericlagergren/decimal v0.0.0-20181231230500-73749d4874d5
+replace github.com/ericlagergren/decimal => github.com/ericlagergren/decimal v0.0.0-20181231230500-73749d4874d5
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eliona-smart-building-assistant/go-eliona-api-client/v2 v2.4.14 // indirect
+	github.com/friendsofgo/errors v0.9.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.1 // indirect
+	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/volatiletech/inflect v0.0.1 // indirect
+	github.com/volatiletech/strmangle v0.0.4 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+)
